f4d96bbbf2d99068a0a4b5dd0bc35557a2417614
wesamsaleh <wesamssaleh@gmail.com>
2021-09-11 23:43:03 -0400

    Added java test file to make sure git works

---
+++ src/hello.java
@@ -0,0 +1,10 @@
+/**
+ * @author Wesam Saleh
+ * Test File to make sure GIT works (THIS FILE IS TO BE DELETED LATER)
+ */
+
+public class hello {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
