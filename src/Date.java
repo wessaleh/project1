16ac0e44deedec9248ae8e39b2ce6ab9a0edd8f7
wesamsaleh <wesamssaleh@gmail.com>
2021-09-19 19:20:53 -0400

    Wrote the isValid method and overrode the toString method for Date class

--- src/Date.java
+++ src/Date.java
@@ -1,4 +1,6 @@
 /**
+ * Date class that stores a given date in the format "mm/dd/yy"
+ * This date class can also check if the date is valid according to certain specifications
  * @author Wesam Saleh
  */
 
@@ -10,12 +12,17 @@ public class Date implements Comparable<Date>{
     private int month;
     private int day;
 
-    public static final int QUADRENNIAL = 4;
-    public static final int CENTENNIAL = 200;
-    public static final int QUARTERCENTENNIAL = 400;
-    public static final int THE_EIGHTYS = 1980;
-    public static final String[] months = new String[] {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
+    private static final int QUADRENNIAL = 4;
+    private static final int CENTENNIAL = 100;
+    private static final int QUARTER_CENTENNIAL = 400;
+    private static final int THE_EIGHTIES = 1980;
+    private static final int MINIMUM_DAY_VALUE = 1;
+    private static final int MINIMUM_MONTH_VALUE = 1;
+    private static final int NUMBER_OF_MONTHS_IN_A_YEAR = 12;
+    private static final String[] MONTHS = new String[] {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
             "Oct", "Nov", "Dec"};
+    private static final int[] DAYS_OF_MONTH = new int[] {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private static final int[] DAYS_OF_MONTH_IN_LEAP_YEAR = new int[] {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     /**
      * take "mm/dd/yyyy" and create a Date object
@@ -51,8 +58,8 @@ public class Date implements Comparable<Date>{
     private static String convertMonthCodeToNumber(String month){
         String monthNumber = "";
 
-        for(int i=0; i<months.length; i++){
-            if(month.equals(months[i])){
+        for(int i=0; i<MONTHS.length; i++){
+            if(month.equals(MONTHS[i])){
                 monthNumber = (i+1)/10 + "" + (i+1)%10; // to ensure single digits have a 0 in front
             }
         }
@@ -67,6 +74,46 @@ public class Date implements Comparable<Date>{
      * @return - true if the date is valid, false if not
      */
     public boolean isValid() {
+        Date currentDate = new Date();
+
+        // validating year
+        if(this.year < THE_EIGHTIES || this.year > currentDate.year){
+            return false;
+        }
+
+        // validating month
+        if(this.month < MINIMUM_MONTH_VALUE || this.month > NUMBER_OF_MONTHS_IN_A_YEAR){
+            return false;
+        }
+
+        // validating day
+        int monthIndex = month-1;
+        int numDaysInThisMonth = isLeapYear(this.year) ? DAYS_OF_MONTH_IN_LEAP_YEAR[monthIndex] :
+                DAYS_OF_MONTH[monthIndex];
+
+        if(this.day < MINIMUM_DAY_VALUE || this.day > numDaysInThisMonth){
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Checks if a given year is a leap year
+     * @param year - the year
+     * @return - true if the year is a leap year, false if not
+     */
+    private static boolean isLeapYear(int year){
+        boolean isDivisibleByFour = year % QUADRENNIAL == 0;
+        boolean isDivisibleBy100 = year % CENTENNIAL == 0;
+        boolean isDivisibleBy400 = year % QUARTER_CENTENNIAL == 0;
+
+        if(isDivisibleByFour){
+            if(isDivisibleBy100){
+                return isDivisibleBy400;
+            }
+            return true;
+        }
         return false;
     }
 
@@ -78,7 +125,6 @@ public class Date implements Comparable<Date>{
      * date passed in>
      */
     public int compareTo(Date date) {
-
         if(this.year-date.year != 0){ // check year first
             return this.year-date.year;
         }else if(this.month-date.month != 0){ // then check month
@@ -86,9 +132,20 @@ public class Date implements Comparable<Date>{
         }else if(this.day-date.day != 0){ // then check day
             return this.day-date.day;
         }else {
-            return 0; // otherwise the two dates are equal
+            return 0; // otherwise, the two dates are equal
         }
+    }
 
+    @Override
+    /**
+     * returns the Date in string form
+     */
+    public String toString(){
+        String monthInStringForm = this.month / 10 + "" + this.month % 10;
+        String dayInStringForm = this.day / 10 + "" + this.day % 10;
+        String yearInStringForm = this.year + "";
+
+        return monthInStringForm + "/" + dayInStringForm + "/" + yearInStringForm;
     }
 
     /**
